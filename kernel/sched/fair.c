6253c29f604c2726452ab3ea1874d2707a3b8238
idkwhoiam322 <snkm1999@gmail.com>
2019-01-06 07:28:37 +0530

    Add sched_set/get_wake_up_idle functions for qcacld dependency
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2648,6 +2648,27 @@ static inline void update_cfs_shares(struct sched_entity *se)
 #endif /* CONFIG_FAIR_GROUP_SCHED */
 
 #ifdef CONFIG_SMP
+u32 sched_get_wake_up_idle(struct task_struct *p)
+{
+	u32 enabled = p->flags & PF_WAKE_UP_IDLE;
+
+	return !!enabled;
+}
+EXPORT_SYMBOL(sched_get_wake_up_idle);
+
+int sched_set_wake_up_idle(struct task_struct *p, int wake_up_idle)
+{
+	int enable = !!wake_up_idle;
+
+	if (enable)
+		p->flags |= PF_WAKE_UP_IDLE;
+	else
+		p->flags &= ~PF_WAKE_UP_IDLE;
+
+	return 0;
+}
+EXPORT_SYMBOL(sched_set_wake_up_idle);
+
 /*
  * Approximate:
  *   val * y^n,    where y^32 ~= 0.5 (~1 scheduling period)
