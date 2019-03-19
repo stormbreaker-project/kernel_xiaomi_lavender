1e320693dfc00257e874d693b55b2a4ab140956d
Danny Lin <danny@kdrag0n.dev>
2019-03-18 17:58:48 -0700

    lpm-levels: Allow enforcing deepest cluster idle state selection
    Similarly to the use_deepest_state flag added to cpuidle in commit
    d29563d2fd4f ("cpuidle: Allow enforcing deepest idle state selection"),
    add a cluster_use_deepest_state flag to lpm-levels that forces the
    cluster idle state to be the deepest possible state at the time of
    selection. Note that this is *not* always the deepest state available as
    the status of RPM will influence whether the system is able to enter the
    deepest state(s).
    
    This flag can be modified from within the kernel as well as from
    userspace through a module parameter.
    
    The deepest cluster idle state available on sdm845 uses 76% less power
    than the shallowest idle state.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Yasir-Siddiqui <www.mohammad.yasir.s@gmail.com>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -81,6 +81,11 @@ module_param_named(
 	tmr_add, tmr_add, uint, S_IRUGO | S_IWUSR | S_IWGRP
 );
 
+static uint32_t ref_premature_cnt = 1;
+
+static bool cluster_use_deepest_state;
+module_param(cluster_use_deepest_state, bool, 0664);
+
 struct lpm_history {
 	uint32_t resi[MAXSAMPLES];
 	int mode[MAXSAMPLES];
@@ -149,6 +154,11 @@ void lpm_suspend_wake_time(uint64_t wakeup_time)
 		suspend_wake_time = wakeup_time;
 }
 EXPORT_SYMBOL(lpm_suspend_wake_time);
+
+void lpm_cluster_use_deepest_state(bool enable)
+{
+	cluster_use_deepest_state = enable;
+}
 
 static uint32_t least_cluster_latency(struct lpm_cluster *cluster,
 					struct latency_level *lat_level)
@@ -956,6 +966,24 @@ static void clear_cl_predict_history(void)
 	}
 }
 
+static int cluster_select_deepest(struct lpm_cluster *cluster)
+{
+	int i;
+
+	for (i = cluster->nlevels - 1; i >= 0; i--) {
+		struct lpm_cluster_level *level = &cluster->levels[i];
+
+		if (level->notify_rpm) {
+			if (level->notify_rpm && msm_rpm_waiting_for_ack())
+				continue;
+		}
+
+		break;
+	}
+
+	return i;
+}
+
 static int cluster_select(struct lpm_cluster *cluster, bool from_idle,
 							int *ispred)
 {
@@ -969,6 +997,9 @@ static int cluster_select(struct lpm_cluster *cluster, bool from_idle,
 
 	if (!cluster)
 		return -EINVAL;
+
+	if (cluster_use_deepest_state)
+		return cluster_select_deepest(cluster);
 
 	sleep_us = (uint32_t)get_cluster_sleep_time(cluster, NULL,
 						from_idle, &cpupred_us);
