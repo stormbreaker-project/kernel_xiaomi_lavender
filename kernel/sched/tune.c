1803463e930bdda6c71b41bacf3aee6493df02ea
idkwhoiam322 <snkm1999@gmail.com>
2019-06-20 20:28:46 +0300

    sched/tune: stune_assist: selectively block some processes and improve code
    This makes it block some userspace processes like init,
    PowerHAL (libmperfmgr - and future/past proof it), etc. while
    allowing users to modify at their own will.
    Kanged from https://github.com/kdrag0n/proton_bluecross/commit/03b087e3bc5a74051f265e12d9c823d6556da487
    
    Also changed from usage of strcmp to memcmp.
    
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>
    Signed-off-by: nysascape <jago@nysascape.digital>
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -694,7 +694,9 @@ boost_write(struct cgroup_subsys_state *css, struct cftype *cft,
 static int boost_write_wrapper(struct cgroup_subsys_state *css,
 			struct cftype *cft, s64 boost)
 {
-	if (!strcmp(current->comm, "init"))
+	if (!memcmp(current->comm, "init", sizeof("init")) ||
+		!memcmp(current->comm, "NodeLooperThrea", sizeof("NodeLooperThrea")) ||
+		!memcmp(current->comm, "power@", sizeof("power@")))
 		return 0;
 
 	boost_write(css, NULL, boost);
@@ -705,7 +707,7 @@ static int boost_write_wrapper(struct cgroup_subsys_state *css,
 static int prefer_idle_write_wrapper(struct cgroup_subsys_state *css,
 			struct cftype *cft, u64 prefer_idle)
 {
-	if (!strcmp(current->comm, "init"))
+	if (!memcmp(current->comm, "init", sizeof("init")))
 		return 0;
 
 	prefer_idle_write(css, NULL, prefer_idle);
