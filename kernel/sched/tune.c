6c5e41d140fd7a7bf98e059ca547f12f6262eb96
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-08-07 19:40:41 +0530

    sched: tune: remove stune_assist
    Will pick latest.
    
    This reverts commits:
    0d706a9dfaca24d229e3db31b9e95d3011de5ab7
    726b06c46b78a216037e7c7028f62eeec3c129b4
    60db47e622aea382a5453f2e8f706789ea20c5cd
    10c2c07efdf55ecee47f866e4eba26fff35eb476
    3ff747489cb4d0df375d27b050c763afb162109e
    22264bec5bd38964993c52d2ef85018a5e59699a
    
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>
    Signed-off-by: nysascape <jago@nysascape.digital>
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -690,50 +690,16 @@ boost_write(struct cgroup_subsys_state *css, struct cftype *cft,
 	return 0;
 }
 
-#ifdef CONFIG_STUNE_ASSIST
-static int boost_write_wrapper(struct cgroup_subsys_state *css,
-			struct cftype *cft, s64 boost)
-{
-	if (!memcmp(current->comm, "init", sizeof("init")) ||
-		!memcmp(current->comm, "NodeLooperThrea", sizeof("NodeLooperThrea")) ||
-		!memcmp(current->comm, "power@", sizeof("power@")))
-		return 0;
-
-	boost_write(css, NULL, boost);
-
-	return 0;
-}
-
-static int prefer_idle_write_wrapper(struct cgroup_subsys_state *css,
-			struct cftype *cft, u64 prefer_idle)
-{
-	if (!memcmp(current->comm, "init", sizeof("init")))
-		return 0;
-
-	prefer_idle_write(css, NULL, prefer_idle);
-
-	return 0;
-}
-#endif
-
 static struct cftype files[] = {
 	{
 		.name = "boost",
 		.read_s64 = boost_read,
-#ifdef CONFIG_STUNE_ASSIST
-		.write_s64 = boost_write_wrapper,
-#else
 		.write_s64 = boost_write,
-#endif
 	},
 	{
 		.name = "prefer_idle",
 		.read_u64 = prefer_idle_read,
-#ifdef CONFIG_STUNE_ASSIST
-		.write_u64 = prefer_idle_write_wrapper,
-#else
 		.write_u64 = prefer_idle_write,
-#endif
 	},
 	{ }	/* terminate */
 };
@@ -756,30 +722,6 @@ schedtune_boostgroup_init(struct schedtune *st, int idx)
 	st->idx = idx;
 }
 
-#ifdef CONFIG_STUNE_ASSIST
-static void write_default_values(struct cgroup_subsys_state *css)
-{
-	u8 i;
-	struct groups_data {
-		char *name;
-		int boost;
-		bool prefer_idle;
-	};
-
-	struct groups_data groups[3] = {
-		{ "top-app",	1, 1 },
-		{ "foreground", 0, 1 },
-		{ "background", 0, 0 }};
-
-	for (i = 0; i < ARRAY_SIZE(groups); i++) {
-		if (!strcmp(css->cgroup->kn->name, groups[i].name)) {
-			boost_write(css, NULL, groups[i].boost);
-			prefer_idle_write(css, NULL, groups[i].prefer_idle);
-		}
-	}
-}
-#endif
-
 static struct cgroup_subsys_state *
 schedtune_css_alloc(struct cgroup_subsys_state *parent_css)
 {
@@ -795,13 +737,10 @@ schedtune_css_alloc(struct cgroup_subsys_state *parent_css)
 		return ERR_PTR(-ENOMEM);
 	}
 
-	for (idx = 1; idx < BOOSTGROUPS_COUNT; ++idx) {
+	/* Allow only a limited number of boosting groups */
+	for (idx = 1; idx < BOOSTGROUPS_COUNT; ++idx)
 		if (!allocated_group[idx])
 			break;
-#ifdef CONFIG_STUNE_ASSIST
-		write_default_values(&allocated_group[idx]->css);
-#endif
-	}
 	if (idx == BOOSTGROUPS_COUNT) {
 		pr_err("Trying to create more than %d SchedTune boosting groups\n",
 		       BOOSTGROUPS_COUNT);
