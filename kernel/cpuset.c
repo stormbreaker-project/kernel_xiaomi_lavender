0339152be69f0522e0056f351c114913d1d10c1d
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-09-24 23:20:53 +0530

    cpusets: tune a bit
    Signed-off-by: nysascape <jago@nysascape.digital>
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -1771,10 +1771,10 @@ static ssize_t cpuset_write_resmask_wrapper(struct kernfs_open_file *of,
 #ifdef CONFIG_CPUSETS_ASSIST
 	static struct cs_target cs_targets[] = {
 		{ "audio-app",		"1-2" },
-		{ "background",		"0-3" },
+		{ "background",		"0-1" },
 		{ "camera-daemon",	"0-3,6-7" },
 		{ "foreground",		"0-3" },
-		{ "restricted",		"0-1" },
+		{ "restricted",		"0-3" },
 		{ "system-background",	"0-3" },
 		{ "top-app",		"0-7" },
 	};
