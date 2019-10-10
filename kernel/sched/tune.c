b4ff4367dd28cb511541ce75eafa61b2d0fdf636
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-10-10 22:43:50 +0530

    sched: tune: set default top-app stune to 5
    Seems to help with both performance and battery.
    
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>
    Signed-off-by: nysascape <jago@nysascape.digital>
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -755,7 +755,7 @@ static void write_default_values(struct cgroup_subsys_state *css)
 		{ "background",	0, 0 },
 		{ "foreground",	0, 0 },
 		{ "rt",		0, 0 },
-		{ "top-app",	2, 1 },
+		{ "top-app",	5, 1 },
 	};
 	int i;
 
