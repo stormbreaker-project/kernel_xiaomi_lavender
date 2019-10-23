b628385353db32274cc076f54167fcba4aa2b85d
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-10-23 18:40:27 +0530

    cpusets: tune foreground and audio-app cpuset for better performance
    Signed-off-by: nysascape <jago@nysascape.digital>
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -1784,10 +1784,10 @@ static ssize_t cpuset_write_resmask_wrapper(struct kernfs_open_file *of,
 {
 #ifdef CONFIG_CPUSETS_ASSIST
 	static struct cs_target cs_targets[] = {
-		{ "audio-app",		"1-2" },
+		{ "audio-app",		"0-3,6-7" },
 		{ "background",		"0-1" },
 		{ "camera-daemon",	"0-3,6-7" },
-		{ "foreground",		"0-3" },
+		{ "foreground",		"0-3,6-7" },
 		{ "restricted",		"0-3" },
 		{ "system-background",	"0-3" },
 		{ "top-app",		"0-7" },
