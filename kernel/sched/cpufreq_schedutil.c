c385c5ea7d2124df16f76d114987df4fa1f22358
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-12-08 22:31:30 +0300

    schedutil: Properly add add task_is_booster() protection to schedutil tuneables
    Signed-off-by: nysascape <jago@nysascape.digital>
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -510,6 +510,9 @@ static ssize_t up_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
 
+	if (task_is_booster(current))
+		return count;
+
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
 
@@ -529,6 +532,9 @@ static ssize_t down_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
+
+	if (task_is_booster(current))
+		return count;
 
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
@@ -556,6 +562,9 @@ static ssize_t iowait_boost_enable_store(struct gov_attr_set *attr_set,
 {
 	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
 	bool enable;
+
+	if (task_is_booster(current))
+		return count;
 
 	if (kstrtobool(buf, &enable))
 		return -EINVAL;
