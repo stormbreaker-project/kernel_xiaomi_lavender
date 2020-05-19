f83d35cd7b8d1d47bd6138f4b7058f20bff34598
sohamxda7 <sensoham135@gmail.com>
2020-05-19 12:17:09 +0000

    Circle Initiate
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,26 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 echo "Cloning dependencies"
-git clone --depth=1 -b old-cam https://github.com/stormbreaker-project/android_kernel_xiaomi_lavender kernel
+git clone --depth=1 https://github.com/stormbreaker-project/android_kernel_xiaomi_lavender -b oldcam-rebase kernel
 cd kernel
-git clone --depth=1 https://github.com/crDroidMod/android_prebuilts_clang_host_linux-x86_clang-6032204 clang
-git clone --depth=1 https://github.com/KudProject/arm-linux-androideabi-4.9 gcc32
-git clone --depth=1 https://github.com/KudProject/aarch64-linux-android-4.9 gcc
-git clone https://github.com/sohamxda7/Anykernel3.git -b lavender --depth=1 AnyKernel
+git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
+git clone --depth=1 https://github.com/sohamxda7/AnyKernel3 AnyKernel
 echo "Done"
-KERNEL_DIR=$(pwd)
-IMAGE="${KERNEL_DIR}/out/arch/arm64/boot/Image.gz-dtb"
-TANGGAL=$(date +"%Y%m%d-%H")
-BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-PATH="${KERNEL_DIR}/clang/bin:${KERNEL_DIR}/gcc/bin:${KERNEL_DIR}/gcc32/bin:${PATH}"
-export KBUILD_COMPILER_STRING="$(${KERNEL_DIR}/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
+IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
+TANGGAL=$(date +"%F-%S")
+START=$(date +"%s")
+export CONFIG_PATH=$PWD/arch/arm64/configs/lavender-perf_defconfig
+PATH="${PWD}/clang/bin:$PATH"
 export ARCH=arm64
-export KBUILD_BUILD_USER=sohamsen
 export KBUILD_BUILD_HOST=circleci
+export KBUILD_BUILD_USER="sohamsen"
 # sticker plox
 function sticker() {
     curl -s -X POST "https://api.telegram.org/bot$token/sendSticker" \
-        -d sticker="CAACAgUAAxkBAAJNIl44MHAX4Kg_6opRvrIM0PNjcLZfAAKwAAN24FYTSoIYNjisXxwYBA" \
-        -d chat_id="$chat_id"
+        -d sticker="CAADBQADVAADaEQ4KS3kDsr-OWAUFgQ" \
+        -d chat_id=$chat_id
 }
 # Send info plox channel
 function sendinfo() {
@@ -28,14 +25,17 @@ function sendinfo() {
         -d chat_id="$chat_id" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=html" \
-        -d text="New build available!%0ADevice : <code>Xiaomi Redmi Note 7/7S</code>%0AToolchain : <code>${KBUILD_COMPILER_STRING}</code>%0ABranch : <code>${BRANCH}</code>%0ACommit Point : <code>$(git log --pretty=format:'"%h : %s"' -1)</code>"
+        -d text="<b>• Predator-Stormbreaker Kernel •</b>%0ABuild started on <code>Circle CI</code>%0AFor device <b>Xiaomi Redmi Note7/7S</b> (lavender)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b>#Stable"
 }
 # Push kernel to channel
 function push() {
-    cd AnyKernel || exit 1
+    cd AnyKernel
     ZIP=$(echo *.zip)
     curl -F document=@$ZIP "https://api.telegram.org/bot$token/sendDocument" \
-        -F chat_id="$chat_id"
+        -F chat_id="$chat_id" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Xiaomi Redmi Note 7/7s (lavender)</b> | <b>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
 }
 # Fin Error
 function finerr() {
@@ -46,39 +46,27 @@ function finerr() {
         -d text="Build throw an error(s)"
     exit 1
 }
-# Build Success
-function buildsucs() {
-    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-        -d chat_id="$chat_id" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=markdown" \
-        -d text="Build Success. Congratulations!"
-}
 # Compile plox
 function compile() {
-    make -j$(nproc) O=out ARCH=arm64 lavender-perf_defconfig
-    make -j$(nproc) O=out \
-                    ARCH=arm64 \
-                    CC=clang \
-                    CLANG_TRIPLE=aarch64-linux-gnu- \
-                    CROSS_COMPILE=aarch64-linux-android- \
-                    CROSS_COMPILE_ARM32=arm-linux-androideabi-
-
-    if ! [ -a "$IMAGE" ]; then
-        finerr
-        exit 1
-    fi
-    buildsucs
-    cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+   make O=out ARCH=arm64 lavender-perf_defconfig
+       make -j$(nproc --all) O=out \
+                             ARCH=arm64 \
+			     CC=clang \
+			     CROSS_COMPILE=aarch64-linux-gnu- \
+			     CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+   cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 }
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 Predator-Stormbreaker-${TANGGAL}.zip *
-    cd ..
+    zip -r9 Predator-Stormbreaker-lavender-${TANGGAL}.zip *
+    cd .. 
 }
-compile
-zipping
 sticker
 sendinfo
+compile
+zipping
+END=$(date +"%s")
+DIFF=$(($END - $START))
 push
+
