1e76f0acc0ffea3fb9539f3e90b1e10b401c547e
sohamxda7 <sensoham135@gmail.com>
2020-05-27 20:35:48 +0000

    CI Changes
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Cloning dependencies"
-git clone --depth=1 https://github.com/stormbreaker-project/android_kernel_xiaomi_lavender -b oldcam-rebase kernel
+git clone --depth=1 https://github.com/stormbreaker-project/android_kernel_xiaomi_lavender -b rebase kernel
 cd kernel
 git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
 git clone --depth=1 https://github.com/sohamxda7/AnyKernel3 AnyKernel
