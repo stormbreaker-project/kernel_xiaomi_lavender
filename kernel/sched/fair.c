ecde3ade8e610f05d397359b4635199d3879c1ce
Tyler Nijmeh <tylernij@gmail.com>
2020-06-23 00:05:30 -0700

    sched: Reduce the opportunity for sleepers to preempt
    Require the current task to be surpassing the new task by 5ms instead of
    1ms before preemption occurs. Reduce jitter due to less frequent task
    interruptions.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -104,8 +104,8 @@ unsigned int __read_mostly sysctl_sched_energy_aware = 1;
  * and reduces their over-scheduling. Synchronous workloads will still
  * have immediate wakeup/sleep latencies.
  */
-unsigned int sysctl_sched_wakeup_granularity = 2000000UL;
-unsigned int normalized_sysctl_sched_wakeup_granularity = 2000000UL;
+unsigned int sysctl_sched_wakeup_granularity		= 5000000UL;
+unsigned int normalized_sysctl_sched_wakeup_granularity	= 5000000UL;
 
 const_debug unsigned int sysctl_sched_migration_cost = 500000UL;
 
